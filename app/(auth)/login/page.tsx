3cd67f26a2d74210351bc77d2820b66ecb73aa3b
ghavisho <ghavishosho@gmail.com>
2026-08-13 14:28:03 -0700

    Delete app/(auth) directory

--- app/(auth)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getPreferences } from "@/lib/theme/preferences";
-import { LoginForm } from "@/components/auth/LoginForm";
-
-export default function LoginPage() {
-  const { locale } = getPreferences();
-  return <LoginForm locale={locale} />;
-}
--- app/(auth)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getPreferences } from "@/lib/theme/preferences";
-import { RegisterForm } from "@/components/auth/RegisterForm";
-
-export default function RegisterPage() {
-  const { locale } = getPreferences();
-  return <RegisterForm locale={locale} />;
-}
